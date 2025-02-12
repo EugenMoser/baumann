46346503431ff03a0752dc3c246d0f54b82bf08d
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2025-02-12 14:49:33 +0100

    create non-binding request button

--- src/components/ProductInfoSection.tsx
+++ src/components/ProductInfoSection.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { addThousendSeperator } from "@/lib/utils";
+import { MouseEventHandler } from "react";
+
+import {
+  addThousendSeperator,
+  sendInquiry,
+} from "@/lib/utils";
 import {
   ArticleProps,
   ColorProps,
@@ -11,68 +16,67 @@ interface ProductInfoSectionProps {
   selectedColor: ColorProps | undefined;
 }
 
+function getAllFeatures(article: ArticleProps): React.JSX.Element {
+  const descriptions: (string | null)[] = [
+    article.description1,
+    article.description2,
+    article.description3,
+    article.description4,
+  ].filter(Boolean);
+
+  return (
+    <>
+      {descriptions.length > 0 && (
+        <>
+          <h2>Besonderheiten</h2>
+          <ul>
+            {descriptions.map((description: string | null) => (
+              <li key={article.id + description}>{description}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
+}
+
+function getAllVPE(article: ArticleProps): React.JSX.Element {
+  const vpe: (string | null)[] = [
+    article.vpe1,
+    article.vpe2,
+    article.vpe3,
+    article.vpe4,
+  ].filter(Boolean);
+
+  return (
+    <>
+      {vpe.length > 0 && (
+        <>
+          <h2>Mögliche Verpackungseinheiten (VPE):</h2>
+          <ul>
+            {vpe.map((vpe: string | null) => (
+              <li key={article.id + vpe}>{addThousendSeperator(vpe)} Stück</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
+}
+
 function ProductInfoSection({
   selectedArticle,
   selectedColor,
 }: ProductInfoSectionProps): React.JSX.Element {
-  const articleNumber = selectedArticle?.number;
-  const colorSuffix = selectedColor?.colorSuffix;
+  if (!selectedArticle)
+    return <p>bitte wählen Sie einen Artikel und eine Farbe aus</p>;
 
   // if suffix is 0, dont show suffix
-  const articleWithColorNumber =
-    colorSuffix !== 0 ? `${articleNumber}-${colorSuffix}` : articleNumber;
+  const articleWithColorNumber: string =
+    selectedColor?.colorSuffix !== 0
+      ? `${selectedArticle.number}-${selectedColor?.colorSuffix}`
+      : selectedArticle.number;
 
-  function getAllFeatures(article: ArticleProps): React.JSX.Element {
-    const descriptions: (string | null)[] = [
-      article.description1,
-      article.description2,
-      article.description3,
-      article.description4,
-    ].filter(Boolean);
-
-    return (
-      <>
-        {descriptions.length > 0 && (
-          <>
-            <h2>Besonderheiten</h2>
-            <ul>
-              {descriptions.map((description: string | null) => (
-                <li key={article.id + description}>{description}</li>
-              ))}
-            </ul>
-          </>
-        )}
-      </>
-    );
-  }
-
-  function getAllVPE(article: ArticleProps): React.JSX.Element {
-    const vpe: (string | null)[] = [
-      article.vpe1,
-      article.vpe2,
-      article.vpe3,
-      article.vpe4,
-    ].filter(Boolean);
-
-    return (
-      <>
-        {vpe.length > 0 && (
-          <>
-            <h2>Mögliche Verpackungseinheiten (VPE):</h2>
-            <ul>
-              {vpe.map((vpe: string | null) => (
-                <li key={article.id + vpe}>
-                  {addThousendSeperator(vpe)} Stück
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </>
-    );
-  }
-
-  //todo create non-binding request
   return (
     <div>
       <h2>Produktinfos</h2>
@@ -81,10 +85,10 @@ function ProductInfoSection({
           <p>Artikelnummer: {articleWithColorNumber}</p>
           {getAllFeatures(selectedArticle)}
           {getAllVPE(selectedArticle)}
+          <button onClick={() => sendInquiry(articleWithColorNumber)}>
+            Unverbindlich anfragen
+          </button>
         </>
-      )}
-      {!selectedArticle && (
-        <p>bitte wählen Sie einen Artikel und eine Farbe aus</p>
       )}
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,13 @@ export function addThousendSeperator(value: string | number | null) {
   if (!value) return null;
   return Number(value).toLocaleString("de-DE");
 }
+
+export function sendInquiry(articleWithColorNumber: string): void {
+  // Hier musst du die E-Mail-Adresse und den Inhalt der Anfrage definieren
+  const recipientEmail = "deine-email-adresse@example.com";
+  const subject = "Unverbindliche Anfrage für Produkt";
+  const body = `Hallo, ich möchte gerne unverbindlich das Produkt ${articleWithColorNumber} anfragen.`;
+
+  // Hier kannst du die Anfrage per E-Mail senden
+  window.location.href = `mailto:${recipientEmail}?subject=${subject}&body=${body}`;
+}
