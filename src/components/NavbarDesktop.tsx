87421ebf5956014831dc52f77579398e284d2f5c
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2025-07-18 22:00:48 +0200

    debug active button background

--- src/components/NavbarDesktop.tsx
+++ src/components/NavbarDesktop.tsx
@@ -16,16 +16,18 @@ export default function NavbarDesktop({
   pathname,
 }: NavbarDesktopProps): React.JSX.Element {
   return (
-    /* Desktop Navigation */
     <NavigationMenuList className="hidden w-full max-w-full list-none justify-around gap-4 lg:flex">
       {productCategories.map((category) => (
         <NavigationMenuItem
           key={category.category}
           className={clsx(
-            "bg-navbar-itemBackground flex h-[36px] w-24 min-w-fit grow cursor-pointer items-center justify-center rounded-md bg-navbar-item p-2 hover:bg-navbar-hover hover:text-navbar dark:hover:bg-navbar-hover",
+            "flex h-[36px] w-24 min-w-fit grow cursor-pointer items-center justify-center rounded-md p-2",
+
             {
               "bg-navbar-active text-navbar underline":
-                pathname === category.href,
+                pathname === category.href, // Active state
+              "bg-navbar-itemBackground hover:bg-navbar-hover hover:text-navbar dark:hover:bg-navbar-hover":
+                pathname !== category.href, // Hover only when not active
             },
           )}
         >
